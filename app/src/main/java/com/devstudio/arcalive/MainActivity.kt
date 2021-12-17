ca58cd87ceadb79c3529c2c0baf30cfab3e47699
OFox <38041066+OFox213@users.noreply.github.com>
2021-12-17 12:14:26 +0900

    commit

--- app/src/main/java/com/devstudio/arcalive/MainActivity.kt
+++ app/src/main/java/com/devstudio/arcalive/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devstudio.arcalive
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.devstudio.arcalive.ui.fragmet.MainFragmentStateAdapter
 
@@ -14,7 +15,14 @@ class MainActivity : AppCompatActivity() {
         val viewPager2 = findViewById<ViewPager2>(R.id.mainViewpager)
         viewPager2.adapter = MainFragmentStateAdapter(this)
         //viewPager2.isUserInputEnabled = false  스와이프 막기
-        
 
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if(position == 1){
+                    viewPager2.isUserInputEnabled = false
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/devstudio/arcalive/ui/fragmet/ArcaViewFragment.kt
+++ app/src/main/java/com/devstudio/arcalive/ui/fragmet/ArcaViewFragment.kt
@@ -1,11 +1,20 @@
 package com.devstudio.arcalive.ui.fragmet
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Message
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.LinearLayout
 import com.devstudio.arcalive.R
+import java.net.URISyntaxException
 
 class ArcaViewFragment : Fragment() {
 
@@ -15,6 +24,45 @@ class ArcaViewFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_arca_view, container, false)
+
+        val webView = v.findViewById<WebView>(R.id.arcaWebView)
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+
+                if(webView.url.equals("https://arca.live/channels")){
+                    webView.loadUrl("https://arca.live")
+                }
+
+                if(newProgress >= 100) {
+                    webView.visibility = View.VISIBLE
+                    v.findViewById<LinearLayout>(R.id.loadingLayout).visibility = View.GONE
+                }
+            }
+
+            override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+                return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg)
+
+            }
+        }
+        webView.webViewClient = object: WebViewClient(){
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                webView.loadUrl(url)
+                return false
+            }
+        }
+        val webSettings:WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.setSupportMultipleWindows(true)
+        webSettings.loadWithOverviewMode = true
+        webSettings.setSupportZoom(true)
+        webSettings.builtInZoomControls = true
+        webSettings.cacheMode = WebSettings.LOAD_DEFAULT
+        webSettings.domStorageEnabled = true
+        webView.loadUrl("https://arca.live")
+
+
         return v
     }
 }
